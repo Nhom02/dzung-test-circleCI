f5f977c29933da32aa4840bb51a9fd63179bb9c4
dungnguyend <102780084+dungnguyend@users.noreply.github.com>
2022-09-19 11:51:38 +0700

    Add test sh bash scripts

---
+++ e2e/scripts/ci-testsuite.runner.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+helpFunction()
+{
+  echo ""
+  echo "Usage: $0 -a PARAMTESTENV -b PARAMTESTSLUG -c PARAMTESTSUITE"
+  echo -e "\t-a PARAMTESTENV\t: test environment dev, qa, prod"
+  echo -e "\t-b PARAMTESTSLUG\t: test slug name"
+  echo -e "\t-c PARAMTESTSUITE\t: test suite name"
+  exit 1 # Exit script after printing help
+}
+
+while getopts "a:b:c:d:" opt
+do
+  case "$opt" in
+    a ) PARAMTESTENV="$OPTARG" ;;
+    b ) PARAMTESTSLUG="$OPTARG" ;;
+    c ) PARAMTESTSUITE="$OPTARG" ;;
+    ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+  esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$PARAMTESTENV" ] || [ -z "$PARAMTESLUG" ] || [ -z "$PARAMTESTSUITE" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct
+echo ""
+echo "Test Environment:\t $PARAMTESTENV"
+echo "Test Slug Name :\t $PARAMTESLUG"
+echo "Test Suite Name :\t $PARAMTESTSUITE"
+echo "Test Working Dir:\t $PWD"
+
+# Setup - create a particular test specs for the particular suite
+echo ""
+echo "1. Setup - Assign a particular test specs for the particular test suite"
+# Define test suite master-data, planning
+SPECS_MASTER_DATA="**/e2e/manage/**/*.feature"
+SPECS_OPTIMISE="**/e2e/optimise/**/*.feature"
+SPECS_TRACKING="**/e2e/live_tracking/**/*.feature"
+SPECS_PLANNING="!(**/e2e/(manage|optimise|live_tracking)/**/*).feature"
+
+if [ "$PARAMTESTSUITE" = "master-data" ]; then
+  CYPRESS_SPECS="$SPECS_MASTER_DATA"
+elif [ "$PARAMTESTSUITE" = "optimise" ]; then
+  CYPRESS_SPECS="$SPECS_OPTIMISE"
+elif [ "$PARAMTESTSUITE" = "tracking" ]; then
+  CYPRESS_SPECS="$SPECS_TRACKING"
+elif [ "$PARAMTESTSUITE" = "planning" ]; then
+  CYPRESS_SPECS="$SPECS_PLANNING"
+else
+  echo "\tERROR: \"$PARAMTESTSUITE\" test suite was not defined!"
+  exit 1 # Exit script if test suite was not defined!
+fi
+
+# Setup - Make the cucumber-json-formatter file executable
+echo "2. Setup - Make the cucumber-json-formatter file executable"
+chmod +x ./tools/cucumber/linux/cucumber-json-formatter
+
+# Run cypress
+echo "3. Starting - Run Cypress! $CYPRESS_SPECS"
+
+# CYPRESS_E2E_TEST_SUITE="$PARAMTESTSUITE" \
+#   ./node_modules/@nrwl/cli/bin/nx.js e2e explore-e2e \
+#   --configuration "$PARAMTESTENV" \
+#   --e2e \
+#   --spec "$CYPRESS_SPECS"
+detox test -c android --loglevel trace --record-videos all
+echo "Debug $?"
+
+# Force - Exit code 0 - with echo at the last command
+echo "Finished - Run Cypress!"
