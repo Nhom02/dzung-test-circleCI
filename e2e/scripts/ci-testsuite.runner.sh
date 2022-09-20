7a52818968b7410500491fa0ca850c0121486e90
dungnguyend <102780084+dungnguyend@users.noreply.github.com>
2022-09-20 15:49:34 +0700

    Update ci-testsuite.runner.sh

--- e2e/scripts/ci-testsuite.runner.sh
+++ e2e/scripts/ci-testsuite.runner.sh
@@ -41,6 +41,9 @@ SPECS_MASTER_DATA="**/e2e/manage/**/*.feature"
 SPECS_OPTIMISE="**/e2e/optimise/**/*.feature"
 SPECS_TRACKING="**/e2e/live_tracking/**/*.feature"
 SPECS_PLANNING="!(**/e2e/(manage|optimise|live_tracking)/**/*).feature"
+RANDOMSTRING = `"$RANDOM" | md5sum | head -c 20`;
+echo $RANDOMSTRING;
+echo "$RANDOMSTRING";
 echo $RANDOM; echo;
 echo $RANDOM | md5sum | head -c 20; echo;
 echo md5sum | head -c 20; echo;
